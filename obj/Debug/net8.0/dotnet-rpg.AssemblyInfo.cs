2f4e4d3de036a6114c2dac5d40ce4c236fe5c10d
manshree <bhoyarmanshree2001@gmail.com>
2024-07-02 15:33:48 +0530

    file path changed

--- obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb5a6d5a3e84b06000bb63da5a3e934aa0d3cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
